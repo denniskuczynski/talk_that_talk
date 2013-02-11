a6a6cbeb68f5d0db333e9fcb90d96010d3e6102e
Dennis Kuczynski <dennis.kuczynski@gmail.com>
2013-02-11 11:52:57 -0500

    Heroku settings change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ TalkThatTalk::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-  config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css)
+  config.assets.compile = false
+  config.assets.precompile += %w( active_admin.css.scss active_admin.js)
 
   # Generate digests for assets URLs
   config.assets.digest = true
